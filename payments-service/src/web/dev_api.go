92f175dc7c50cc0d369acbfba3db82c3820699f2
blazejwylegly <blazej.wylegly@gmail.com>
2023-11-30 22:22:47 +0100

    [feature] Added basics for payments service

---
+++ payments-service/src/web/dev_api.go
@@ -0,0 +1,36 @@
+package web
+
+import (
+	"github.com/blazejwylegly/transactions-poc/payments-service/src/config"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type DevApi struct {
+	apiSubRouter *mux.Router
+	config       config.Config
+}
+
+func InitDevApi(router *mux.Router, config config.Config) *DevApi {
+	devApiRouter := router.PathPrefix("/dev/config").Subrouter()
+	api := &DevApi{
+		apiSubRouter: devApiRouter,
+		config:       config,
+	}
+	api.initializeMappings()
+	return api
+}
+
+func (api DevApi) initializeMappings() {
+	api.apiSubRouter.HandleFunc("", api.handleGetConfig()).Methods("GET")
+}
+
+func (api DevApi) handleGetConfig() func(http.ResponseWriter, *http.Request) {
+	return func(response http.ResponseWriter, request *http.Request) {
+		encoder := defaultEncoder(response)
+		err := encoder.Encode(api.config)
+		if err != nil {
+			http.Error(response, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
