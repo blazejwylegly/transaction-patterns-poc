ea0f17d21efe75857400c91c6892bb494a6d2656
blazejwylegly <blazej.wylegly@gmail.com>
2023-12-13 21:54:24 +0100

    [feature] Modified entity mapping for tx tracking

--- tx-tracker/src/db/entities.go
+++ tx-tracker/src/db/entities.go
@@ -9,11 +9,12 @@ type Transaction struct {
 	TxnId     uuid.UUID `gorm:"type:uuid;column:txn_id;primary_key"`
 	TxnName   string
 	StartedAt time.Time
-	Steps     []TransactionStep `gorm:"foreignKey:StepId"`
+	Steps     []TransactionStep `gorm:"foreignKey:TxnId"`
 }
 
 type TransactionStep struct {
-	StepId       uuid.UUID `gorm:"type:uuid;column:txn_id;primary_key"`
+	StepId       uuid.UUID `gorm:"type:uuid;column:step_id;primary_key"`
+	TxnId        uuid.UUID
 	StepName     string
 	StepExecutor string
 	Payload      string `gorm:"type:jsonb" json:"payload"`
